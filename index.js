1af36346750003d9024095645975b68bc5bb41ac
Rick Koster <ngf.koster@outlook.com>
2018-01-03 20:53:03 +0100

    feat(core): find the correct entry to add the react-hot-loader/patch module

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
+const path = require('path');
+
+const NO_REACT_PATCH_ERROR =
+    'Unable to include `react-hot-loader/patch`, the entry path was not ' +
+    'found. Please make sure to use the `entry` option in your ' +
+    '`poi.config.js` file.';
+
 module.exports = ({ tsLoaderOptions } = {}) => poi => {
+    // Extract the entry path, used to find which webpack entry imports the
+    // entry path.
+    const entryPath = path.resolve(poi.options.entry);
+
     poi.extendWebpack(config => {
+        // Add ts and tsx extensions as importable extensions
         config.resolve.extensions.add('.ts').add('.tsx');
 
+        // Add ts-loader to ts or tsx files
         config.module
             .rule('typescript')
             .test(/\.tsx?$/)
@@ -14,6 +27,7 @@ module.exports = ({ tsLoaderOptions } = {}) => poi => {
                 Object.assign({ appendTsSuffixTo: [/\.vue$/] }, tsLoaderOptions)
             );
 
+        // Add ts-loader to vue files
         config.module
             .rule('vue')
             .use('vue-loader')
@@ -28,12 +42,21 @@ module.exports = ({ tsLoaderOptions } = {}) => poi => {
                 return vueOptions;
             });
 
-        if (config.entryPoints.has('client')) {
-            config
-                .entry('client')
-                .prepend(require.resolve('react-hot-loader/patch'));
-        } else {
-            throw new Error('Currently only the `client` entry is supported');
+        // Find the entry that includes the entry path and prepend the entry
+        // with react-hot-loader/patch.
+        let patchIncluded = false;
+        for (const key of config.entryPoints.store.keys()) {
+            const entry = config.entry(key);
+
+            if (entry.store.has(entryPath)) {
+                entry.prepend(require.resolve('react-hot-loader/patch'));
+                patchIncluded = true;
+            }
+        }
+
+        // Throw an error if the entry path is not found.
+        if (!patchIncluded) {
+            throw new Error(NO_REACT_PATCH_ERROR);
         }
     });
 };
