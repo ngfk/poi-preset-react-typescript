6bd8142248faa3fa02049e0f39fd2481ab1bbf49
Rick Koster <ngf.koster@outlook.com>
2018-01-03 09:03:03 +0100

    feat(core): implement poi plugin

---
+++ index.js
@@ -0,0 +1,41 @@
+module.exports = ({ tsLoaderOptions } = {}) => poi => {
+    poi.extendWebpack(config => {
+        config.resolve.extensions.add('.ts').add('.tsx');
+
+        config.module
+            .rule('typescript')
+            .test(/\.tsx?$/)
+            .use('react-hot-loader/webpack')
+            .loader('react-hot-loader/webpack')
+            .end()
+            .use('ts-loader')
+            .loader('ts-loader')
+            .options(
+                Object.assign({ appendTsSuffixTo: [/\.vue$/] }, tsLoaderOptions)
+            );
+
+        console.log(config.module.rules.store.get('typescript'));
+
+        config.module
+            .rule('vue')
+            .use('vue-loader')
+            .tap(vueOptions => {
+                vueOptions.esModule = true;
+                vueOptions.loaders.ts = [
+                    {
+                        loader: 'ts-loader',
+                        options: tsLoaderOptions
+                    }
+                ];
+                return vueOptions;
+            });
+
+        if (config.entryPoints.has('client')) {
+            config
+                .entry('client')
+                .prepend(require.resolve('react-hot-loader/patch'));
+        } else {
+            throw new Error('Currently only the `client` entry is supported');
+        }
+    });
+};
