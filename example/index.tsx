c23538b9bdb215d67a857d06340831fff6ed906d
Rick Koster <ngf.koster@outlook.com>
2018-04-22 17:56:08 +0200

    docs(example): retrieve container element from within main

--- example/index.tsx
+++ example/index.tsx
@@ -2,27 +2,28 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
 
-const root = document.getElementById('app');
-
-const render = (Component: any) => {
+const render = (Component: any, container: HTMLElement) => {
     ReactDOM.render(
         <AppContainer>
             <Component />
         </AppContainer>,
-        root
+        container
     );
 };
 
 const main = async () => {
+    const container = document.getElementById('app');
+    if (!container) throw new Error('No element with id "app" found');
+
     // Initial render
     const { App: InitialApp } = await import('./containers/App');
-    render(InitialApp);
+    render(InitialApp, container);
 
     // Re-render on hot module updates
     if (module.hot) {
         module.hot.accept('./containers/App', async () => {
             const { App: NextApp } = await import('./containers/App');
-            render(NextApp);
+            render(NextApp, container);
         });
     }
 };
